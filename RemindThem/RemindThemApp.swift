20100054f2aefa3a142638555b3ec60ffd27e6a9
JackmanParker <113531688+JackmanParker@users.noreply.github.com>
2024-03-06 23:41:44 -0700

    Initial Commit

---
+++ RemindThem/RemindThemApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindThemApp.swift
+//  RemindThem
+//
+//  Created by Parker Jackman on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct RemindThemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
