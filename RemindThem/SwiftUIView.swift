952c7417892463ca404ee1085f983059d70042ed
JackmanParker <113531688+JackmanParker@users.noreply.github.com>
2024-03-23 23:13:54 -0600

    Updated the detaileditView

---
+++ RemindThem/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  RemindThem
+//
+//  Created by Parker Jackman on 3/16/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
